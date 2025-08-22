09872df7090b87ed3593dffdfda44252b1ee9589
Lejdy <denamusic123@gmail.com>
2025-08-22 23:29:17 +0000

    commit 1

--- Kalkulacka.py
+++ Kalkulacka.py
@@ -1,10 +1,50 @@
+def vypočítaj():
+    číslo1 = input("Zadaj prvé číslo: ")
+    def prvé_číslo():
+        while True:
+            try:
+                num1 = int(číslo1)
+                return num1
+            except ValueError:
+                print("Nevieš čítať? Máš zadať číslo!")
 
-# Online Python - IDE, Editor, Compiler, Interpreter
+    znamienka = input ("Zvoľ si operáciu prostredníctvom znakov +, -, *, /: ")
+    def operácia():
+        while True:
+            if znamienka in ["+" or "-" or "*" or "/"]:
+                return znamienka
+            else:
+                print("To akože nevieš čítať? Môžeš zadať iba znaky +, -, *, /.")
+    
+    číslo2 = input("Zadaj prvé číslo: ")
+    def druhé_číslo():
+        while True:
+            try:
+                num2 = int(číslo2)
+                return num2
+            except ValueError:
+                print("Nevieš čítať? Máš zadať číslo!")
 
-def sum(a, b):
-    return (a + b)
+    print("Vytaj v tejto úžastnej kalkulačke.")
+    prvý_krok = prvé_číslo()
+    druhý_krok = operácia()
+    tretí_krok = druhé_číslo()
+    
+    a = prvé_číslo()
+    b = druhé_číslo()
+    if znamienka == ["+"]:
+        v = a + b
+        print(v)
+    if znamienka == ["-"]:
+        x = a - b
+        print(x)
+    if znamienka == ["*"]:
+        y = a * b
+        print(y)
+    if znamienka == ["/"]:
+        z = a / b
+        print("z")
 
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
+štvrtý_krok = vypočítaj()
 
-print(f'Sum of {a} and {b} is {sum(a, b)}')
+    
