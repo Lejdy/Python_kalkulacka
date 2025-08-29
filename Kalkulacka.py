1cc9d3706b7628ba6f11777962cd9ee045cfae3c
Lejdy <denamusic123@gmail.com>
2025-08-29 14:42:10 +0000

    commit 2

--- Kalkulacka.py
+++ Kalkulacka.py
@@ -1,50 +1,44 @@
-def vypočítaj():
-    číslo1 = input("Zadaj prvé číslo: ")
-    def prvé_číslo():
+print("Vitaj v tejto kalkulačke!")
+def operácia():
+    def číslo1():
         while True:
+            user_input = input("Zadaj prvé číslo tvojho príkladu: ")
             try:
-                num1 = int(číslo1)
-                return num1
+                num = float(user_input)
+                return num
             except ValueError:
                 print("Nevieš čítať? Máš zadať číslo!")
+    num1 = číslo1()
 
-    znamienka = input ("Zvoľ si operáciu prostredníctvom znakov +, -, *, /: ")
-    def operácia():
+    def číslo2():
         while True:
-            if znamienka in ["+" or "-" or "*" or "/"]:
-                return znamienka
-            else:
-                print("To akože nevieš čítať? Môžeš zadať iba znaky +, -, *, /.")
-    
-    číslo2 = input("Zadaj prvé číslo: ")
-    def druhé_číslo():
-        while True:
+            user_input = input("Zadaj druhé číslo tvojho príkladu: ")
             try:
-                num2 = int(číslo2)
-                return num2
+                num = float(user_input)
+                return num
             except ValueError:
                 print("Nevieš čítať? Máš zadať číslo!")
+    num2 = číslo2()
 
-    print("Vytaj v tejto úžastnej kalkulačke.")
-    prvý_krok = prvé_číslo()
-    druhý_krok = operácia()
-    tretí_krok = druhé_číslo()
-    
-    a = prvé_číslo()
-    b = druhé_číslo()
-    if znamienka == ["+"]:
-        v = a + b
-        print(v)
-    if znamienka == ["-"]:
-        x = a - b
-        print(x)
-    if znamienka == ["*"]:
-        y = a * b
-        print(y)
-    if znamienka == ["/"]:
-        z = a / b
-        print("z")
-
-štvrtý_krok = vypočítaj()
-
-    
+    user_input = input("Zvoľ si operáciu zadaním znaku operácie (+, -, *, /): ")
+    a = num1
+    b = num2
+    if user_input == "+":
+        príklad = a + b
+        print("Výsledok je", príklad)
+    elif user_input == "-":
+        príklad = a - b
+        print("Výsledok je", príklad)
+    elif user_input == "*":
+        príklad = a * b
+        print("Výsledok je", príklad)
+    elif user_input == "/":
+        if num2 == 0:
+            print("Nulou sa nedá deliť!")
+        else:
+            príklad = a / b
+            print("Výsledok je", príklad)
+    else:
+        print("Zadal si nesprávny znak operácie, príklad ti tak neviem vypočítať.")
+        
+operácia()
